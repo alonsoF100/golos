3764d06ef8babb34d6d4a2a7ac67ca1e88eb0c4c
AleshaPustota <4247334@mail.ru>
2025-12-01 03:31:35 +0300

    handlers and dto + bit service and errors

--- internal/erorrs/errors.go
+++ internal/erorrs/errors.go
@@ -3,8 +3,17 @@ package apperrors
 import "errors"
 
 var (
-	ErrUserAlreadyExist = errors.New("user aready exist")
-	ErrUserNotFound     = errors.New("user not found")
-	ErrNothingToChange = errors.New("nothing to change")
+	// User errors
+	ErrUserAlreadyExist     = errors.New("user already exist")
+	ErrUserNotFound         = errors.New("user not found")
+	ErrNothingToChange      = errors.New("nothing to change")
 	ErrFailedToHashPassword = errors.New("failed to hash password")
+
+	// Election errors
+	ErrElectionAlreadyExist = errors.New("election already exist")
+	ErrElectionNotFound     = errors.New("election not found")
+
+	// Vote Variant errors
+	ErrVoteVariantAlreadyExist = errors.New("vote variant already exist")
+	ErrVoteVariantNotFound     = errors.New("vote variant not found")
 )
--- internal/transport/http/dto/response.go
+++ internal/transport/http/dto/response.go
@@ -58,3 +58,86 @@ func NewUsersResponse(users []*models.User) UsersResponse {
 
 	return responseUsers
 }
+
+// // election dto
+type ElectionResponse struct {
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func NewElectionResponse(election *models.Election) ElectionResponse {
+	return ElectionResponse{
+		ID:          election.ID,
+		UserID:      election.UserID,
+		Name:        election.Name,
+		Description: election.Description,
+		CreatedAt:   election.CreatedAt,
+		UpdatedAt:   election.UpdatedAt,
+	}
+}
+
+type ElectionsResponse struct {
+	Elections []*ElectionResponse
+}
+
+func NewElectionsResponse(elections []*models.Election) ElectionsResponse {
+	responseElections := ElectionsResponse{
+		Elections: make([]*ElectionResponse, 0, len(elections)),
+	}
+	for _, election := range elections {
+		temp := &ElectionResponse{
+			ID:          election.ID,
+			UserID:      election.UserID,
+			Name:        election.Name,
+			Description: election.Description,
+			CreatedAt:   election.CreatedAt,
+			UpdatedAt:   election.UpdatedAt,
+		}
+		responseElections.Elections = append(responseElections.Elections, temp)
+	}
+	return responseElections
+}
+
+// Vote Variant Responses
+type VoteVariantResponse struct {
+	ID         string    `json:"id"`
+	ElectionID string    `json:"election_id"`
+	Name       string    `json:"name"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+func NewVoteVariantResponse(voteVariant *models.VoteVariant) VoteVariantResponse {
+	return VoteVariantResponse{
+		ID:         voteVariant.ID,
+		ElectionID: voteVariant.ElectionID,
+		Name:       voteVariant.Name,
+		CreatedAt:  voteVariant.CreatedAt,
+		UpdatedAt:  voteVariant.UpdatedAt,
+	}
+}
+
+type VoteVariantsResponse struct {
+	VoteVariants []*VoteVariantResponse
+}
+
+func NewVoteVariantsResponse(voteVariants []*models.VoteVariant) VoteVariantsResponse {
+	responseVariants := VoteVariantsResponse{
+		VoteVariants: make([]*VoteVariantResponse, 0, len(voteVariants)),
+	}
+	for _, variant := range voteVariants {
+		temp := &VoteVariantResponse{
+			ID:         variant.ID,
+			ElectionID: variant.ElectionID,
+			Name:       variant.Name,
+			CreatedAt:  variant.CreatedAt,
+			UpdatedAt:  variant.UpdatedAt,
+		}
+		responseVariants.VoteVariants = append(responseVariants.VoteVariants, temp)
+	}
+	return responseVariants
+}
